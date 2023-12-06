fb9c9f3edef18bb10d1a40ce387cc668ba8dfce5
Tonny-1999 <oduortonny10@gmail.com>
2023-12-06 12:59:29 +0300

    add delete user functionality

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -3,7 +3,11 @@ import { useSelector } from 'react-redux';
 import { useRef } from "react";
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
 import { app } from '../firebase';
+<<<<<<< HEAD
 import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+=======
+import { updateUserStart, updateUserSuccess, updateUserFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure } from '../redux/user/userSlice';
+>>>>>>> 6c7f958 (add delete user functionality)
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
@@ -79,6 +83,41 @@ const handleFileUpload = (file) => {
    }
  };
 
+<<<<<<< HEAD
+=======
+ const handleDeleteUser = async () => {
+   try {
+     dispatch(deleteUserStart());
+     const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+       method: 'DELETE',
+     });
+     const data = await res.json();
+     if (data.success === false) {
+       dispatch(deleteUserFailure(data.message));
+       return;
+     }
+     dispatch(deleteUserSuccess(data));
+   } catch (error) {
+     dispatch(deleteUserFailure(error.message));
+   }
+ };
+
+ const handleSignOut = async () => {
+   try {
+     dispatch(signOutUserStart());
+     const res = await fetch('/api/auth/signout');
+     const data = await res.json();
+     if (data.success === false) {
+       dispatch(deleteUserFailure(data.message));
+       return;
+     }
+     dispatch(deleteUserSuccess(data));
+   } catch (error) {
+     dispatch(deleteUserFailure(data.message));
+   }
+ };
+
+>>>>>>> 6c7f958 (add delete user functionality)
   return (
     <div className='p-3 max-w-lg mx-auto'>
        <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -107,8 +146,8 @@ const handleFileUpload = (file) => {
 
        </form>
        <div className='flex justify-between mt-5'> 
-          <span className='text-red-700 cursor-pointer'>Delete Account</span>
-          <span className='text-red-700 cursor-pointer'>Sign Out</span>
+          <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
+          <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign Out</span>
        </div>
         <p className='text-red-700 mt-5'>{error ? error : ''}</p>
         <p className='text-green-700 mt-5'>{updateSuccess ? 'User Successfully updated' : ''}</p>
