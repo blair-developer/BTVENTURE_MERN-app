8805c3cad8343af29d3b49166778badf68667ea2
Tonny-1999 <oduortonny10@gmail.com>
2024-01-14 11:36:22 +0300

    updates

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,11 +4,13 @@ export default function About() {
   return (
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Sahand Estate</h1>
-      <p className='mb-4 text-slate-700'>Sahand Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+      <p className='mb-4 text-slate-700'>   
+      Welcome to BTVenture, a leading innovator in the realm of software engineering. With a commitment to excellence and a passion for cutting-edge technology, we strive to redefine the possibilities of digital solutions.
+        </p>
       <p className='mb-4 text-slate-700'>
-      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+      At BTventure, our mission is to empower businesses and individuals by crafting bespoke software solutions that not only meet but exceed their expectations. We believe in leveraging the power of technology to drive innovation, efficiency, and sustainable growth.
       </p>
-      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
     </div>
   )
 }
